cece3bfd1bd83b4f4339be0c02cd290427a0ee1a
amir.mofid@apsaaz.com <amir.mofid@apsaaz.com>
2023-10-29 12:50:00 +0330

    initializa

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+/* better way of importing component*/
+
+
+export {default as AddContact} from "./Contacts/AddContact";
+export {default as EditContact} from "./Contacts/EditContact";
+export {default as ViewContact} from "./Contacts/ViewContact";
+export {default as Contact} from "./Contacts/Contact";
+export {default as Contacts} from "./Contacts/Contacts";
+export {default as SearchContact} from "./Contacts/SearchContact";
+
+export {default as Navbar} from "./Navbar";
+export {default as Spinner} from "./Spinner";
